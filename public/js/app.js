79b1ac125f8f0bb7a673c9641c416d910f12ff98
Tomaz Dobrisek <tomaz@dobrisek.si>
2016-08-11 01:15:15 +0200

    changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 	var map = null;
 	var mapMarkers = [];
+	var scanMarker = null;
+	var cellMarkers = [];
 	var locationMarker = null;
 
 	var socket = null;
@@ -10,58 +12,32 @@
 
 		socket = io();
 
-		socket.on("search-results", function(list, finish){
-			console.log(list, finish);
+		socket.on("search-results", function(list, finish, type){
+			console.log(list, finish, type);
 
 			if( finish === true){
 				$("#indicator").hide();
 			}
 			
-
-			if( list && list.length > 0){
-				$.each(list, function(i, pokemon){
-					
-					if( pokemon.Type === "map" || pokemon.Type === "wild" ){
-
-						var title = pokemon.Type == "wild" ?  'Wild '+ pokemon.name+" - "+ time2Str(pokemon.Time): pokemon.name;
-
-						var markerImage = new google.maps.MarkerImage("/img/"+pokemon.num+".png",
-			                new google.maps.Size(50, 50),
-			                new google.maps.Point(0, 0),
-			                new google.maps.Point(25, 25)
-			            );
-
-			            var infowindow = new google.maps.InfoWindow({
-						    content: title
-						  });
-
-						var marker = new google.maps.Marker({
-					          position: { lat: pokemon.Latitude, lng: pokemon.Longitude },
-					          map: map,
-					          title: title,
-					          icon: markerImage
-					    });
-
-					    marker.addListener("click", function(){
-					    	infowindow.open(map, marker);
-					    });
-
-						mapMarkers.push( marker);
-
-					}else if( pokemon.lat && pokemon.lng){
-						// debug
-						mapMarkers.push( new google.maps.Marker({
-					          position: { lat: pokemon.lat, lng: pokemon.lng },
-					          map: map,
-					          title: "point",
-					        })
-				        );
+			switch(type){
+				case "pokemon":
+					if( list && list.length > 0){
+						showPokemon(list);
 					}
-
-					
-
-				});
+					break;
+				case "scan":
+					if( list ){
+						showScanLocation(list);
+					}
+					break;
+				case "cell":
+					if( list && list.length > 0){
+						showCells(list);
+					}
+					break;
 			}
+
+
 		});
 
 		$("#map").height($(window).height());
@@ -123,6 +99,16 @@
 			m.setMap(null);
 		});
 		mapMarkers = [];
+
+		cellMarkers.forEach(function(m){
+			m.setMap(null);
+		});
+		cellMarkers = [];
+
+		if( scanMarker !== null ){
+			scanMarker.setMap(null);
+			scanMarker = null;
+		}
 	}
 
 	function search(){
@@ -142,4 +128,90 @@
 		}else{
 			return "(might not be visible yet)";
 		}
+	}
+
+	function showPokemon(list){
+		$.each(list, function(i, pokemon){
+			
+			if( pokemon.Type === "map" || pokemon.Type === "wild" ){
+
+				var title = pokemon.Type == "wild" ?  'Wild '+ pokemon.name+" - "+ time2Str(pokemon.Time): pokemon.name;
+
+				var markerImage = new google.maps.MarkerImage("/img/"+pokemon.num+".png",
+	                new google.maps.Size(50, 50),
+	                new google.maps.Point(0, 0),
+	                new google.maps.Point(25, 25)
+	            );
+
+	            var infowindow = new google.maps.InfoWindow({
+				    content: title
+				  });
+
+				var marker = new google.maps.Marker({
+			          position: { lat: pokemon.Latitude, lng: pokemon.Longitude },
+			          map: map,
+			          title: title,
+			          icon: markerImage
+			    });
+
+			    marker.addListener("click", function(){
+			    	infowindow.open(map, marker);
+			    });
+
+				mapMarkers.push( marker);
+
+			}/*else if( pokemon.lat && pokemon.lng){
+				// debug
+				mapMarkers.push( new google.maps.Marker({
+			          position: { lat: pokemon.lat, lng: pokemon.lng },
+			          map: map,
+			          title: "point",
+			        })
+		        );
+			}*/
+
+		});
+	}
+
+	function showScanLocation(pos){
+		if( scanMarker !== null){
+			scanMarker.setMap(null);
+			scanMarker = null;
+		}
+
+		scanMarker = new google.maps.Circle({
+	            strokeColor: '#00FF00',
+	            strokeOpacity: 0.5,
+	            strokeWeight: 2,
+	            fillColor: '#00FF00',
+	            fillOpacity: 0.20,
+	            map: map,
+	            center: pos,
+	            radius: 100
+	    });
+	}
+
+	function showCells(list){
+		return;
+
+
+		cellMarkers.forEach(function(m){
+			m.setMap(null);
+		});
+		cellMarkers = [];
+
+		$.each(list, function(i, pos){
+			var marker = new google.maps.Circle({
+	            strokeColor: '#0000FF',
+	            strokeOpacity: 0.8,
+	            strokeWeight: 2,
+	            fillColor: '#0000FF',
+	            fillOpacity: 0.35,
+	            map: map,
+	            center: pos,
+	            radius: 10
+	          });
+
+			cellMarkers.push(marker);
+		});
 	}
